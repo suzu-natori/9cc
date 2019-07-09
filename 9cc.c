e612412392c5f2a8148beca6affc7094778a6398
suzu-natori <suzu.natori.2424@gmail.com>
2019-07-10 00:31:34 +0900

    トークナイザの導入

--- 9cc.c
+++ 9cc.c
@@ -1,40 +1,73 @@
+#include <ctype.h>
 #include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef enum {
+    TK_RESERVED,
+    TK_NUM,
+    TK_EOF,
+} TokenKind;
+
+typedef struct Token Token;
+
+struct Token {
+    TokenKind kind;
+    Token *next;
+    int val;
+    char *str;
+};
+
+Token *token;
+
+bool at_eof(void);
+bool consume(char op);
 void error(char *fmt, ...);
+void expect(char op);
+int expect_number(void);
+Token *new_token(TokenKind kind, Token *cur, char *str);
+Token *tokenize(char *p);
 
 int main(int argc, char **argv) {
     if (argc != 2) {
         error("引数の個数が正しくありません。");
     }
 
-    char *p = argv[1];
+    token = tokenize(argv[1]);
 
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
-    printf("    mov rax, %ld\n", strtol(p, &p, 10));
+    printf("    mov rax, %d\n", expect_number());
 
-    while (*p) {
-        if (*p == '+') {
-            p++;
-            printf("    add rax, %ld\n", strtol(p, &p, 10));
+    while (!at_eof()) {
+        if (consume('+')) {
+            printf("    add rax, %d\n", expect_number());
             continue;
         }
 
-        if (*p == '-') {
-            p++;
-            printf("    sub rax, %ld\n", strtol(p, &p, 10));
+        if (consume('-')) {
+            printf("    sub rax, %d\n", expect_number());
             continue;
         }
-
-        error("予期しない文字です：'%c'\n", *p);
     }
 
     printf("    ret\n");
 
     return EXIT_SUCCESS;
+}
+
+bool at_eof(void) {
+    return token->kind == TK_EOF;
+}
+
+bool consume(char op) {
+    if (token->kind != TK_RESERVED || token->str[0] != op) {
+        return false;
+    }
+    token = token->next;
+    return true;
 }
 
 void error(char *fmt, ...) {
@@ -43,4 +76,57 @@ void error(char *fmt, ...) {
     vfprintf(stderr, fmt, ap);
     fprintf(stderr, "\n");
     exit(EXIT_FAILURE);
+}
+
+void expect(char op) {
+    if (token->kind != TK_RESERVED || token->str[0] == op) {
+        error("'%c'ではありません", op);
+    }
+    token = token->next;
+    return;
+}
+
+int expect_number(void) {
+    if (token->kind != TK_NUM) {
+        error("数ではありません");
+    }
+    int val = token->val;
+    token = token->next;
+    return val;
+}
+
+Token *new_token(TokenKind kind, Token *cur, char *str) {
+    Token *tok = calloc(1, sizeof(Token));
+    tok->kind = kind;
+    tok->str = str;
+    cur->next = tok;
+    return tok;
+}
+
+Token *tokenize(char *p) {
+    Token head;
+    head.next = NULL;
+    Token *cur = &head;
+
+    while (*p) {
+        if (isspace(*p)) {
+            p++;
+            continue;
+        }
+
+        if (*p == '+' || *p == '-') {
+            cur = new_token(TK_RESERVED, cur, p++);
+            continue;
+        }
+        if (isdigit(*p)) {
+            cur = new_token(TK_NUM, cur, p);
+            cur->val = strtol(p, &p, 10);
+            continue;
+        }
+
+        error("トークナイズできません");
+    }
+
+    new_token(TK_EOF, cur, p);
+    return head.next;
 }
